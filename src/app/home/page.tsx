6cba82ba1f674b251bfed14fe14690ce8061472c
dfbsx <werkamdor@gmail.com>
2024-04-09 10:07:58 +0200

    Background image and container to draw aded

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,17 @@
+import { Title } from "@mantine/core";
 import styles from "./page.module.css";
 import Header from "@/components/Header";
-export default function Home(){
-    return(<div className={styles.page}><Header/></div>)
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <Header />
+      <img className={styles.imageBackground} src="gym.jpg" alt="Gym" />
+      <Title className={styles.bannerText} order={1}>
+        It's a good day <br/> to generate a new workout
+      </Title>
+      <div className={styles.generateContainter}>
+    containter
+      </div>
+    </div>
+  );
 }
